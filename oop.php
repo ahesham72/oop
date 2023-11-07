2b5d9f1898f0d8e2fe81007913a338a27d75d53d
ahesham72 <ahesham72@gmail.com>
2023-11-07 21:57:53 +0200

    create pro

---
+++ oop.php
@@ -0,0 +1,22 @@
+<style>
+    body{
+        background-color: lightslategray;
+    }
+</style>
+<?php
+
+
+class AppleDevice {
+
+    public $var;
+    private $tests;
+    protected $test2;
+
+    
+    
+}
+
+$test = new AppleDevice ();
+
+echo $test->var= "ali <br>" ;
+echo "ahmed";
